93d8e3b93d6cee1d6a1c21d89442f56e9d0ed2e3
lovis-ambrose <lovisambrose212@gmail.com>
2024-12-08 23:51:35 +0300

    small fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {NotFoundComponent} from "./pages/not-found/not-found.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 import {TeamComponent} from "./pages/team/team.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {HomeComponent} from "./pages/home/home.component";
-import {Menubar} from "primeng/menubar";
+import {MenubarModule} from "primeng/menubar";
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import {Menubar} from "primeng/menubar";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Menubar,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
